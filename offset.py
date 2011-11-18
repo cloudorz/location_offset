6ba0a0cfc98540483dfea5a718d0dd711141945e
cloud <cloudcry@gmail.com>
2011-11-18 17:22:02 +0800

    go go go

--- offset.py
+++ offset.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-import httplib
+import httplib, math
 
 import tornado.web
 import tornado.database
@@ -11,15 +11,16 @@ from tornado.web import HTTPError
 class OffsetHandler(tornado.web.RequestHandler):
 
     def get(self, lat, lon):
-        try:
-            entry = self.db.get("SELECT * FROM cn_map_offset WHERE lat=%s AND lon=%s", int(float(lat)*100), int(float(lon)*100))
-        except Exception:
-            raise HTTPError(500)
 
-        if not entry:
-            raise HTTPError(404)
+        lat, lon = float(lat), float(lon)
+        entries = self.db.get("SELECT * FROM cn_map_offset WHERE lat=%s AND lon=%s", int(lat*100), int(lon*100))
 
-        self.render_json({'off_x': entry.off_x, 'off_y': entry.off_y})
+        fake_lat, fake_lon = lat, lon
+        if entries:
+            op = OffsetPos(lat, lon, entries[0])
+            fake_lat, fake_lon = op.getFakePos()
+
+        self.render_json({'lat': fake_lat, 'lon': fake_lon})
 
     @property
     def db(self):
@@ -35,3 +36,37 @@ class OffsetHandler(tornado.web.RequestHandler):
     def render_json(self, data, **kwargs):
         self.set_header('Content-Type', 'Application/json; charset=UTF-8')
         self.write(json_encode(data))
+
+class OffsetPos(object):
+
+    def __init__(self, lat, lon, entry, zoom=18):
+        self.lat, self.lon = lat, lon 
+        self.off_x, self.off_y = entry.off_x, entry.off_y
+        self.zoom = zoom
+
+    def getFakePos(self):
+        lat_pixel, lon_pixel = self.lat2pixel(), self.lon2pixel()
+
+        lat_pixel += self.off_y
+        lon_pixel += self.off_x
+
+        return self.pixel2lat(lat_pixel), self.pixel2lon(lon_pixel)
+
+    def lat2pixel(self):
+        siny = math.sin(self.lat*math.pi / 180)
+        y = math.log((1 + siny) / (1 - siny))
+
+        return (128 << self.zoom) * (1 - y / (2*math.pi))
+
+    def lon2pixel(self):
+        return (self.lon + 180) * (256 << self.zoom) / 360
+    
+    def pixel2lat(self, pixelY):
+        y = 2 * math.pi * (1 - pixelY / (128 << self.zoom))
+        z = math.pow(math.e, y)
+        siny = (z - 1) / (z + 1)
+
+        return math.asin(siny) * 180 /math.pi
+
+    def pixel2lon(self, pixelX):
+        return pixelX * 360 / (256 << self.zoom) - 180
